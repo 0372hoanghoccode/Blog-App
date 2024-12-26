33eb2f01bb1bb9a91b44b3fd169d66bdd27aef99
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-12-26 23:14:01 +0700

    push

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,127 @@
+'use client'
+import { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage'
+import { app } from '../firebase'
+import { Progress } from '@/components/ui/progress'
+import { Alert, AlertDescription } from '@/components/ui/alert'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
 export default function DashProfile() {
-    return (
-      <div>DashProfile</div>
+  const { currentUser } = useSelector((state) => state.user)
+  const [imageFile, setImageFile] = useState(null)
+  const [imageFileUrl, setImageFileUrl] = useState(null)
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
+  const [imageFileUploadError, setImageFileUploadError] = useState(null)
+  const filePickerRef = useRef()
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      setImageFile(file)
+      setImageFileUrl(URL.createObjectURL(file))
+    }
+  }
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage()
+    }
+  }, [imageFile])
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null)
+    const storage = getStorage(app)
+    const fileName = new Date().getTime() + imageFile.name
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, imageFile)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setImageFileUploadProgress(progress.toFixed(0))
+      },
+      (error) => {
+        setImageFileUploadError(
+          'Could not upload image (File must be less than 2MB)'
+        )
+        setImageFileUploadProgress(null)
+        setImageFile(null)
+        setImageFileUrl(null)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL)
+        })
+      }
     )
   }
+
+  return (
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <Progress
+              value={imageFileUploadProgress}
+              className="absolute inset-0"
+            />
+          )}
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              'opacity-60'
+            }`}
+          />
+        </div>
+        {imageFileUploadError && (
+          <Alert variant="destructive">
+            <AlertDescription>{imageFileUploadError}</AlertDescription>
+          </Alert>
+        )}
+        <Input
+          type='text'
+          id='username'
+          placeholder='username'
+          defaultValue={currentUser.username}
+        />
+        <Input
+          type='email'
+          id='email'
+          placeholder='email'
+          defaultValue={currentUser.email}
+        />
+        <Input type='password' id='password' placeholder='password' />
+        <Button type='submit' variant="outline">
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
+  )
+}
+
